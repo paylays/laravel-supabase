fc57a5bf8e447062c7113467ad606289c93a3162
paylays <160083635+paylays@users.noreply.github.com>
2025-11-24 23:24:06 +0800

    perubahan feature/a kedua

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "hello from feature branch"; // perubahan di feature
+    return "welcome from branch feature/a "; // branch feature/a
 });
